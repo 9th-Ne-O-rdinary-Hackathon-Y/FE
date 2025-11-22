4f8076911caa45072569631ba2ee78ddaf93e8e5
gs0428 <lider28@naver.com>
2025-11-22 23:54:04 +0900

    feat: 결과 분석 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import { Route, Routes } from "react-router-dom";
 
 const RootPage = lazy(() => import("@/pages/root/page"));
 const GamePage = lazy(() => import("@/pages/game/page"));
+const ResultPage = lazy(() => import("@/pages/result/page"));
 
 function App() {
   return (
     <Routes>
       <Route index path="/" element={<RootPage />} />
       <Route path="/game" element={<GamePage />} />
+      <Route path="/result" element={<ResultPage />} />
     </Routes>
   );
 }
