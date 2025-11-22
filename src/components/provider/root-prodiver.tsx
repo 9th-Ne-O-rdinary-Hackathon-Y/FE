7b6bd7a901dfa433ea5019423462ddf925cef0f3
gs0428 <lider28@naver.com>
2025-11-23 04:24:00 +0900

    chore: toast 컴포넌트 추가

--- src/components/provider/root-prodiver.tsx
+++ src/components/provider/root-prodiver.tsx
@@ -1,10 +1,17 @@
 import { queryClient } from "@/lib/tanstack";
 import { QueryClientProvider } from "@tanstack/react-query";
 
+import { Toaster } from "../ui/sonner";
+
 interface RootProdiverProps {
   children: React.ReactNode;
 }
 
 export default function RootProdiver({ children }: RootProdiverProps) {
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <Toaster position="top-center" duration={3000} closeButton />
+    </QueryClientProvider>
+  );
 }
